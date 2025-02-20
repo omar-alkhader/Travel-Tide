3ff692d17e21f7ae25816c4d681e3084c6f4dd9c
lil3le <aliobeidat266@gmail.com>
2025-02-20 18:45:37 +0300

    .

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,39 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import Navbar from "./components/Navbar";
-import ProfilePage from "./pages/ProfilePage";
-import ReviewPage from "./pages/ReviewPage";
-import TripDetailsPage from "./pages/TripDetailsPage";
-import PaymentPage from "./pages/PaymentPage";
-import AboutPage from "./pages/AboutPage";
 import Layout from "./components/Layout";
-import GuidePage from "./pages/GuidePage";
+import ProfileForm from "./components/ProfileForm";
+import RatingForm from "./components/RatingForm";
+import TripDetailsForm from "./components/TripDetailsForm";
+import AboutPage from "./pages/AboutPage";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import ForgotPassword from "./pages/ForgotPassword"; 
 
 import "./styles/NavBar.css";
 import "./styles/ProfilePage.css";
 import "./styles/ReviewPage.css";
 import "./styles/TripDetailsPage.css";
 import "./styles/global.css";
-import "./styles/PaymentPage.css"; 
-
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
-      <Route path="/guides" element={<GuidePage />} />
-        <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/review" element={<ReviewPage />} />
-        <Route path="/trip-details" element={<TripDetailsPage />} />
-        <Route path="/pay" element={<PaymentPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/layout" element={<Layout />}/>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<ProfileForm />} />
+          <Route path="/review" element={<RatingForm />} />
+          <Route path="/trip-details" element={<TripDetailsForm />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} /> 
+        </Route>
       </Routes>
     </Router>
   );
 }
-
 
 export default App;
