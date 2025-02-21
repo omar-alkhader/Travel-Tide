0ec1b77b8b412260b3d5358cab8549c570e36b32
Eyad03 <eyadzmansur@gmail.com>
2025-02-21 12:50:19 +0300

    Update App.js

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -8,19 +8,26 @@ import AboutPage from "./pages/AboutPage";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword"; 
+import PaymentPage from "./pages/PaymentPage";
+import GuidePage from "./pages/GuidePage";
 
 import "./styles/NavBar.css";
 import "./styles/ProfilePage.css";
 import "./styles/ReviewPage.css";
 import "./styles/TripDetailsPage.css";
 import "./styles/global.css";
+import "./styles/PaymentPage.css"; 
+import "./styles/GuidePage.css"; 
+
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
+        <Route path="/guides" element={<GuidePage />} />
           <Route index element={<ProfileForm />} />
+          <Route path="/pay" element={<PaymentPage />} />
           <Route path="/review" element={<RatingForm />} />
           <Route path="/trip-details" element={<TripDetailsForm />} />
           <Route path="/about" element={<AboutPage />} />
