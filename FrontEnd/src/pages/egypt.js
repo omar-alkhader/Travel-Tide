06b54a61e046c712874df9b7388dfe0a09ca4a1c
Eyad03 <eyadzmansur@gmail.com>
2025-04-19 14:17:10 +0300

    Update egypt.js

--- FrontEnd/src/pages/egypt.js
+++ FrontEnd/src/pages/egypt.js
@@ -5,23 +5,35 @@ import { Container, Row, Col } from "react-bootstrap";
 // Import images from assets folder
 
 
-import alexandariaImg from "../assets/ALEXANDARIA.jpg";
-
-const egypt_city = [
-    { id: "alexandaria", name: "Alexandaria", image: alexandariaImg },
-
-  ];
-
-
+import { useQuery } from "@tanstack/react-query";
 
 const Egypt = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["city"],
+    queryFn: async () => {
+      const res = await fetch("http://127.0.0.1:6600/api/cities");
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (isError || !data?.data?.cities) {
+    return <div>Failed to load cities.</div>;
+  }
+
+  const egyptCities = data.data.cities.filter(city => city.country_id ===4);
+
   return (
     <Container className="text-center my-5">
       <h2>Choose Your Package</h2>
       <Row className="mt-4">
-        {egypt_city.map((pkg) => (
-          <Col md={4} key={pkg.id} className="d-flex justify-content-center">
-            <EgyptCard id={pkg.id} name={pkg.name} image={pkg.image} />
+        {egyptCities.map((city) => (
+          <Col md={4} key={city.id} className="d-flex justify-content-center">
+            <EgyptCard id={city.id} name={city.name} image={city.photo} />
           </Col>
         ))}
       </Row>
